084291752694d6e267699f0b9550f8449c685860
yfadskch <yfadskch@gmail.com>
2024-12-08 13:25:52 +0800

    1231231aaaa

--- script.js
+++ script.js
@@ -16,30 +16,38 @@ document.addEventListener('DOMContentLoaded', function () {
             element: document.getElementById('gameContainer'),
             engine: engine,
             options: {
-                width: 656,
-                height: 799,
+                width: 768,
+                height: 1365,
                 wireframes: false,
                 background: 'transparent'
             }
         });
 
-        // 创建随机摆放的障碍物
-        var numberOfObstacles = 150;
-        for (let i = 0; i < numberOfObstacles; i++) {
-            let x = Math.random() * render.options.width;
-            let y = Math.random() * (render.options.height - 50) + 50;
-            let obstacle = Bodies.circle(x, y, 10, {
-                isStatic: true,
-                render: { fillStyle: 'black' }
-            });
-            World.add(world, obstacle);
+        // 根据提供的布局创建障碍物
+        var offsetX = 40;  // 左侧开始位置
+        var offsetY = 100; // 顶部开始位置
+        var spacingX = 20; // 水平间距
+        var spacingY = 20; // 垂直间距
+        var rows = 19;     // 总行数
+        var cols = 10;     // 总列数
+
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                let x = offsetX + j * spacingX;
+                let y = offsetY + i * spacingY;
+                let obstacle = Bodies.circle(x, y, 10, {
+                    isStatic: true,
+                    render: { fillStyle: 'black' }
+                });
+                World.add(world, obstacle);
+            }
         }
 
         // 创建小球，尺寸为2px半径
-        var ball = Bodies.circle(328, 30, 2, { 
+        var ball = Bodies.circle(384, 50, 2, {
             density: 0.04,
             friction: 0.01,
-            restitution: 0.8,
+            restitution: 0.8, // 高弹性
             render: { fillStyle: 'red' }
         });
 
